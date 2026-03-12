a8e5e91e4ad8034c4d7b26ea60c7c3df9d6cc83b
daniel <Usuario@DESKTOP-FKV022E>
2026-03-12 09:04:35 -0600

    Lectura de archivos y carga en ComboBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxAlumno = new System.Windows.Forms.ComboBox();
+            this.comboBoxTaller = new System.Windows.Forms.ComboBox();
+            this.buttonInscribir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonMostrar = new System.Windows.Forms.Button();
+            this.buttonOrdenar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Alumno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Taller";
+            // 
+            // comboBoxAlumno
+            // 
+            this.comboBoxAlumno.FormattingEnabled = true;
+            this.comboBoxAlumno.Location = new System.Drawing.Point(122, 32);
+            this.comboBoxAlumno.Name = "comboBoxAlumno";
+            this.comboBoxAlumno.Size = new System.Drawing.Size(620, 24);
+            this.comboBoxAlumno.TabIndex = 2;
+            // 
+            // comboBoxTaller
+            // 
+            this.comboBoxTaller.FormattingEnabled = true;
+            this.comboBoxTaller.Location = new System.Drawing.Point(122, 93);
+            this.comboBoxTaller.Name = "comboBoxTaller";
+            this.comboBoxTaller.Size = new System.Drawing.Size(620, 24);
+            this.comboBoxTaller.TabIndex = 3;
+            // 
+            // buttonInscribir
+            // 
+            this.buttonInscribir.Location = new System.Drawing.Point(363, 142);
+            this.buttonInscribir.Name = "buttonInscribir";
+            this.buttonInscribir.Size = new System.Drawing.Size(131, 38);
+            this.buttonInscribir.TabIndex = 4;
+            this.buttonInscribir.Text = "Inscribir";
+            this.buttonInscribir.UseVisualStyleBackColor = true;
+            this.buttonInscribir.Click += new System.EventHandler(this.buttonInscribir_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(122, 203);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(620, 127);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // buttonMostrar
+            // 
+            this.buttonMostrar.Location = new System.Drawing.Point(122, 416);
+            this.buttonMostrar.Name = "buttonMostrar";
+            this.buttonMostrar.Size = new System.Drawing.Size(187, 38);
+            this.buttonMostrar.TabIndex = 6;
+            this.buttonMostrar.Text = "Mostar reporte";
+            this.buttonMostrar.UseVisualStyleBackColor = true;
+            // 
+            // buttonOrdenar
+            // 
+            this.buttonOrdenar.Location = new System.Drawing.Point(576, 416);
+            this.buttonOrdenar.Name = "buttonOrdenar";
+            this.buttonOrdenar.Size = new System.Drawing.Size(176, 38);
+            this.buttonOrdenar.TabIndex = 7;
+            this.buttonOrdenar.Text = "Ordenar por taller";
+            this.buttonOrdenar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(798, 550);
+            this.Controls.Add(this.buttonOrdenar);
+            this.Controls.Add(this.buttonMostrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonInscribir);
+            this.Controls.Add(this.comboBoxTaller);
+            this.Controls.Add(this.comboBoxAlumno);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxAlumno;
+        private System.Windows.Forms.ComboBox comboBoxTaller;
+        private System.Windows.Forms.Button buttonInscribir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonMostrar;
+        private System.Windows.Forms.Button buttonOrdenar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,5 +20,127 @@ namespace SimulacroExamenParcial
         {
             InitializeComponent();
         }
+
+        private void LeerAlumnos()
+        {
+            FileStream stream = new FileStream("Alumnos.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Alumno alumno = new Alumno();
+
+                alumno.Dpi = reader.ReadLine();
+                alumno.Nombre = reader.ReadLine();
+                alumno.Direccion = reader.ReadLine();
+
+                alumnos.Add(alumno);
+            }
+
+            reader.Close();
+        }
+        private void LeerTalleres()
+        {
+            FileStream stream = new FileStream("Talleres.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Taller taller = new Taller();
+
+                taller.CodigoDelTaller = reader.ReadLine();
+                taller.NombreDelTaller = reader.ReadLine();
+                taller.Costo = Convert.ToDecimal(reader.ReadLine());
+
+                talleres.Add(taller);
+            }
+
+            reader.Close();
+        }
+        private void LeerInscripciones()
+        {
+            FileStream stream = new FileStream("Inscripciones.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Inscripcion inscripcion = new Inscripcion();
+
+                inscripcion.DpiDelEstudiante = reader.ReadLine();
+                inscripcion.CodigoDelTaller = reader.ReadLine();
+                inscripcion.FechaDeInscripcion = Convert.ToDateTime(reader.ReadLine());
+
+                inscripciones.Add(inscripcion);
+            }
+
+            reader.Close();
+        }
+        private void GuardarInscripcion()
+        {
+            FileStream stream = new FileStream("Inscripciones.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+
+            foreach (var ins in inscripciones)
+            {
+                writer.WriteLine(ins.DpiDelEstudiante);
+                writer.WriteLine(ins.CodigoDelTaller);
+                writer.WriteLine(ins.FechaDeInscripcion);
+            }
+
+            writer.Close();
+        }
+        private void MostrarReporte()
+        {
+            var lista = new List<object>();
+
+            foreach (var ins in inscripciones)
+            {
+                Alumno alumno = alumnos.Find(a => a.Dpi == ins.DpiDelEstudiante);
+                Taller taller = talleres.Find(t => t.CodigoDelTaller == ins.CodigoDelTaller);
+
+                lista.Add(new
+                {
+                    Alumno = alumno.Nombre,
+                    Taller = taller.NombreDelTaller,
+                    Fecha = ins.FechaDeInscripcion
+                });
+            }
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = lista;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            LeerAlumnos();
+            LeerTalleres();
+            LeerInscripciones();
+
+            comboBoxAlumno.DataSource = alumnos;
+            comboBoxAlumno.DisplayMember = "Nombre";
+
+            comboBoxTaller.DataSource = talleres;
+            comboBoxTaller.DisplayMember = "NombreDeltaller";
+        }
+
+        private void buttonInscribir_Click(object sender, EventArgs e)
+        {
+            
+            Alumno alumno = (Alumno)comboBoxAlumno.SelectedItem;
+            Taller taller = (Taller)comboBoxTaller.SelectedItem;
+
+            Inscripcion inscripcion = new Inscripcion();
+
+            inscripcion.DpiDelEstudiante = alumno.Dpi;
+            inscripcion.CodigoDelTaller = taller.CodigoDelTaller;
+            inscripcion.FechaDeInscripcion = DateTime.Now;
+
+            inscripciones.Add(inscripcion);
+
+            GuardarInscripcion();
+        }
     }
-}
+    }
+    
+
